5a23a99334bc69130e8edb2248332c69e5718b0a
Pratik Ghongade <pratik.g@goglocal.com>
2023-02-10 11:34:48 +0530

    no message

--- pages/3_DGCI.py
+++ pages/3_DGCI.py
@@ -23,7 +23,7 @@ def convert_df(df):
 try:
     if st.session_state['get']:
         descp = pd.read_csv('mappings/hsn_descp.csv')
-        req_descp =  descp[descp['HSN']==st.session_state['hsn_code_1'].zfill(4)]['DESCP'].tolist()[0]
+        req_descp =  descp[descp['HSN']==int(st.session_state['hsn_code_1'].zfill(4))]['DESCP'].tolist()[0]
         st.write('The Data File corresponding to the HSN code {} belonging to the category {}\nDisplays: {}'.format(st.session_state['hsn_code_1'].zfill(4),st.session_state['category_name_1'],req_descp))
         path = 'dgci_data/'+st.session_state['hsn_code_1'].zfill(4)+'.csv'
         df = pd.read_csv(path)
