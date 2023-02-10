0bb1cdc2362abefe36c6a5f6266c76ad6f919650
Pratik Ghongade <pratik.g@goglocal.com>
2023-02-10 14:51:38 +0530

    no message

--- pages/5_Miscalleneous.py
+++ pages/5_Miscalleneous.py
@@ -15,7 +15,7 @@ st.set_page_config(
     page_title="DataAssetTool"
 )
 
-st.title("Miscalleneous Data Online")
+st.title("Miscalleneous Data Store")
 
 files = []
 for file in os.listdir('jp_morgan_data'):
